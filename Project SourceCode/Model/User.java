2f176fb9a1d174995552a6564afbfd2cf9c6b012
davisallan <davis.allan@ucalgary.ca>
2020-11-26 21:06:27 -0700

    added getter/setters, blank methods per document

--- Project SourceCode/Model/User.java
+++ Project SourceCode/Model/User.java
@@ -13,13 +13,32 @@ public class User{
 	public int getUserId() {
 		return userId;
 	}
-	
-	public void setTicket(Ticket myTicket) {
-		this.ticket = myTicket;
+
+	public Ticket getTicket() {
+		return ticket;
 	}
-	
+
+	public void setTicket(Ticket ticket) {
+		this.ticket = ticket;
+	}
+
+	public Voucher getVoucher() {
+		return voucher;
+	}
+
 	public void setVoucher(Voucher voucher) {
 		this.voucher = voucher;
 	}
-	
+
+	public void makePayment() {
+
+	}
+
+	public void purchaseTicket() {
+
+	}
+
+	public void cancelTicket(Ticket ticket) {
+
+	}
 }
