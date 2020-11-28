df0f46142ecc6862f7c484668fa01512b2cac972
davisallan <davis.allan@ucalgary.ca>
2020-11-28 14:44:13 -0700

    added Auditorium, added datamembers to Theatre

--- Project SourceCode/Model/Theatre.java
+++ Project SourceCode/Model/Theatre.java
@@ -5,18 +5,40 @@ import java.util.ArrayList;
 public class Theatre {
 
 	private String theatreName;
+	private String address;
 	private ArrayList<Movie> movieList;
+	private ArrayList<Auditorium> auditoriums;
 	
 	
-	public Theatre(String name, ArrayList<Movie> movieList) {
-		this.theatreName = name;
-		this.movieList = movieList;
+	public Theatre(String name, String address) {
+		setTheatreName(name);
+		setAddress(address);
+		movieList = new ArrayList<>();
+		auditoriums = new ArrayList<>();
 	}
-	
-	public String getName() {
+
+
+
+	public String getTheatreName() {
 		return theatreName;
 	}
-	
+
+	public void setTheatreName(String theatreName) {
+		this.theatreName = theatreName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public ArrayList<Auditorium> getAuditoriums() {
+		return auditoriums;
+	}
+
 	// get movie list for this theatre
 	public ArrayList<Movie> getMovieList(){
 		return movieList;
