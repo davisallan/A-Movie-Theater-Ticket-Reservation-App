79525595d5dcc59dd2c9c439d71ea865d15ed4cf
davisallan <davis.allan@ucalgary.ca>
2020-11-28 12:22:13 -0700

    created DBCredentials and queries for DBController

--- Project SourceCode/Controller/DBController.java
+++ Project SourceCode/Controller/DBController.java
@@ -1,4 +1,44 @@
 package Controller;
 
-public class DBController {
+import java.sql.*;
+
+public class DBController implements DBCredentials {
+
+    private Connection conn;
+    private ResultSet rs;
+    private PreparedStatement stmt;
+
+    public DBController() {
+        initializeConnection();
+    }
+
+    public void initializeConnection() {
+        try {
+            Driver driver = new com.mysql.cj.jdbc.Driver();
+            DriverManager.registerDriver(driver);
+            conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Problem");
+            e.printStackTrace();
+        }
+    }
+
+    public void close() {
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ResultSet selectAll(String tableName) {
+        try {
+            String query = "SELECT * FROM " + tableName;
+            stmt = conn.prepareStatement(query);
+            rs = stmt.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return rs;
+    }
 }
