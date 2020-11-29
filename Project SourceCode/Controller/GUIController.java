1746679c836f1a1d865114e0cc7633abd8df5a0f
davisallan <davis.allan@ucalgary.ca>
2020-11-29 00:34:07 -0700

    experimenting with GUI

--- Project SourceCode/Controller/GUIController.java
+++ Project SourceCode/Controller/GUIController.java
@@ -1,8 +1,15 @@
 package Controller;
+import View.*;
 
 /**
  * Controller for the GUI
  */
 
 public class GUIController {
+
+    private LoginForm loginForm;
+
+    public GUIController() {
+
+    }
 }
--- Project SourceCode/View/LoginForm.java
+++ Project SourceCode/View/LoginForm.java
@@ -2,10 +2,23 @@ package View;
 
 import javax.swing.*;
 
-public class LoginForm {
+public class LoginForm extends JFrame {
     private JTextField textField1;
     private JButton loginButton;
     private JButton continueAsGuestButton;
     private JButton signUpButton;
     private JPasswordField passwordField1;
+    private JPanel panel;
+
+    public LoginForm() {
+        setContentPane(panel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        LoginForm login = new LoginForm();
+    }
 }
