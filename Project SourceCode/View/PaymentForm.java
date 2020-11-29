48d29a8032af7d09e7b3d214969fdba233f0b366
davisallan <davis.allan@ucalgary.ca>
2020-11-29 04:52:44 -0700

    created payment form

--- Project SourceCode/View/PaymentForm.java
+++ Project SourceCode/View/PaymentForm.java
@@ -1,4 +1,15 @@
 package View;
 
+import javax.swing.*;
+
 public class PaymentForm {
+    private JTextField name;
+    private JTextField ccNum;
+    private JTextField expiry;
+    private JTextField cvc;
+    private JRadioButton creditCardRadioButton;
+    private JRadioButton voucherRadioButton;
+    private JTextField textField1;
+    private JButton submitPaymentButton;
+    private JButton returnToReservationButton;
 }
---
+++ Project SourceCode/View/SeatSelectionForm.java
@@ -0,0 +1,7 @@
+package View;
+
+import javax.swing.*;
+
+public class SeatSelectionForm {
+    private JPanel panel1;
+}
