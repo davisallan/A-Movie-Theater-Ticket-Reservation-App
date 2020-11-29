05cb6611444117d2f31619dc8eb40b9d04056f59
davisallan <davis.allan@ucalgary.ca>
2020-11-29 00:05:15 -0700

    experimenting with GUI

---
+++ Project SourceCode/View/LoginForm.java
@@ -0,0 +1,11 @@
+package View;
+
+import javax.swing.*;
+
+public class LoginForm {
+    private JTextField textField1;
+    private JButton loginButton;
+    private JButton continueAsGuestButton;
+    private JButton signUpButton;
+    private JPasswordField passwordField1;
+}
