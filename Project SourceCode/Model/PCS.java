77f8f6c63da5e025f5db71aacb9673ccd041e307
anorexbill <bryteanorchie1@gmail.com>
2020-11-30 19:47:32 -0700

    Updated payment

--- Project SourceCode/Model/PCS.java
+++ Project SourceCode/Model/PCS.java
@@ -5,30 +5,15 @@ import java.util.ArrayList;
 public class PCS {
 
     private Payment pay;
-
-    private ArrayList paymentList;
-    public PCS(){
-        paymentList = new ArrayList();
-    }
-
-
-
-    public ArrayList getPaymentList() {
-        return paymentList;
-    }
-
-    public void setPaymentList(ArrayList paymentList) {
-        this.paymentList = paymentList;
-    }
-
+    PaymentList payment;
 
     public void createPayment(String name, CreditCard cc, User user) {
         Payment pay = new Payment(user, cc, name);
-        addPayment(pay);
+        payment.addPayment(pay);
     }
 
-    public void addPayment (Payment pay){
-        paymentList.add(pay);
-    }
+//    public void addPayment (Payment pay){
+//        paymentList.add(pay);
+//    }
 }
 
---
+++ Project SourceCode/Model/PaymentList.java
@@ -0,0 +1,17 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class PaymentList {
+
+    private ArrayList<Payment> paymentList;
+
+    public PaymentList(){
+        paymentList = new ArrayList<>();
+    }
+
+
+    public void addPayment(Payment pay){
+        paymentList.add(pay);
+    }
+}
